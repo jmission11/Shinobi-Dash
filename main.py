03a3aa980d372d0f7d35629f53398d9ca6dd3796
Joseph Mission <joseph.mission@bisu.edu.ph>
2024-12-11 21:45:51 +0800

    modify main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import os
 import random 
 import time
 
-from pygame import MOUSEBUTTONDOWN
-from pygame.examples import cursors
 
 pygame.init()
 pygame.mixer.init()  # Initialize the mixer module
@@ -17,42 +15,42 @@ SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 pygame.display.set_caption('Shinobi Dash')
 
 # LOAD AND SCALE RUNNING IMAGES FOR THE NINJA CHARACTER
-RUNNING = [pygame.transform.scale(pygame.image.load(os.path.join(".venv/Assets/Ninja", "ninja1.png")), (130, 130)),
-           pygame.transform.scale(pygame.image.load(os.path.join(".venv/Assets/Ninja", "ninja2.png")), (130, 130))]
+RUNNING = [pygame.transform.scale(pygame.image.load(os.path.join("../.venv/Assets/Ninja", "ninja1.png")), (130, 130)),
+           pygame.transform.scale(pygame.image.load(os.path.join("../.venv/Assets/Ninja", "ninja2.png")), (130, 130))]
 
 # LOAD AND SCALE JUMPING IMAGES FOR THE NINJA CHARACTER
-JUMPING = [pygame.transform.scale(pygame.image.load(os.path.join(".venv/Assets/Ninja", "ninjaJump.png")), (130, 130)),
-           pygame.transform.scale(pygame.image.load(os.path.join(".venv/Assets/Ninja", "ninjaJump2.png")), (130, 130))]
+JUMPING = [pygame.transform.scale(pygame.image.load(os.path.join("../.venv/Assets/Ninja", "ninjaJump.png")), (130, 130)),
+           pygame.transform.scale(pygame.image.load(os.path.join("../.venv/Assets/Ninja", "ninjaJump2.png")), (130, 130))]
 
 # LOAD AND SCALE DUCKING IMAGES FOR THE NINJA CHARACTER
-DUCKING = [pygame.transform.scale(pygame.image.load(os.path.join(".venv/Assets/Ninja", "ninjaDuck1.png")), (119, 100)),
-           pygame.transform.scale(pygame.image.load(os.path.join(".venv/Assets/Ninja", "ninjaDuck2.png")), (119, 100))]
+DUCKING = [pygame.transform.scale(pygame.image.load(os.path.join("../.venv/Assets/Ninja", "ninjaDuck1.png")), (119, 100)),
+           pygame.transform.scale(pygame.image.load(os.path.join("../.venv/Assets/Ninja", "ninjaDuck2.png")), (119, 100))]
 
 # LOAD THE BACKGROUND AND GROUND IMAGES
-BACKGROUND = pygame.image.load(os.path.join(".venv/Assets/Ninja", "background.jpg"))
-GROUND = pygame.image.load(os.path.join(".venv/Assets/Ninja", "ground.png"))
-CLOUDS = pygame.image.load(os.path.join(".venv/Assets/Ninja", "Clouds.png"))
-SUN_IMG = pygame.transform.scale(pygame.image.load(os.path.join(".venv/Assets/Ninja", "sun.png")), (300, 300))
+BACKGROUND = pygame.image.load(os.path.join("../.venv/Assets/Ninja", "background.jpg"))
+GROUND = pygame.image.load(os.path.join("../.venv/Assets/Ninja", "ground.png"))
+CLOUDS = pygame.image.load(os.path.join("../.venv/Assets/Ninja", "Clouds.png"))
+SUN_IMG = pygame.transform.scale(pygame.image.load(os.path.join("../.venv/Assets/Ninja", "sun.png")), (300, 300))
 
 # Load the shuriken image
-SHURIKEN_IMAGE = pygame.transform.scale(pygame.image.load(os.path.join(".venv/Assets/Ninja", "shuriken1.png")), (50, 30))
-KUNAI_IMG = pygame.transform.scale(pygame.image.load(os.path.join(".venv/Assets/Ninja", "kunai.png")), (120, 20))
+SHURIKEN_IMAGE = pygame.transform.scale(pygame.image.load(os.path.join("../.venv/Assets/Ninja", "shuriken1.png")), (50, 30))
+KUNAI_IMG = pygame.transform.scale(pygame.image.load(os.path.join("../.venv/Assets/Ninja", "kunai.png")), (120, 20))
 
-LOGO = pygame.transform.scale(pygame.image.load(os.path.join(".venv/Assets/Ninja", "Logo.png")), (675, 375))
+LOGO = pygame.transform.scale(pygame.image.load(os.path.join("../.venv/Assets/Ninja", "Logo.png")), (675, 375))
 # Load font for Game Over text
 FONT = pygame.font.SysFont('comicsans', 40)
 JUMP_HIGH = 8.5
 
 # Load sound effects and background music
-JUMP_SOUND = pygame.mixer.Sound(os.path.join(".venv/Assets/SoundEffect", "slideClick.mp3"))
-DUCK_SOUND = pygame.mixer.Sound(os.path.join(".venv/Assets/SoundEffect", "woosh.mp3"))
-SHURIKEN_SOUND = pygame.mixer.Sound(os.path.join(".venv/Assets/SoundEffect", "woosh.mp3"))
+JUMP_SOUND = pygame.mixer.Sound(os.path.join("../.venv/Assets/SoundEffect", "slideClick.mp3"))
+DUCK_SOUND = pygame.mixer.Sound(os.path.join("../.venv/Assets/SoundEffect", "woosh.mp3"))
+SHURIKEN_SOUND = pygame.mixer.Sound(os.path.join("../.venv/Assets/SoundEffect", "woosh.mp3"))
 
 # Load game over sound
-GAME_OVER_SOUND = pygame.mixer.Sound(os.path.join(".venv/Assets/Music", "thrash.mp3"))
+GAME_OVER_SOUND = pygame.mixer.Sound(os.path.join("../.venv/Assets/Music", "thrash.mp3"))
 
 # Load background music track
-BACKGROUND_MUSIC = os.path.join(".venv/Assets/Music", "knight.mp3")
+BACKGROUND_MUSIC = os.path.join("../.venv/Assets/Music", "knight.mp3")
 
 def play_background_music():
     GAME_OVER_SOUND.stop()  # Stop the game over music
@@ -87,7 +85,7 @@ class Ninja:
         self.ninja_rect.y = self.y_pos
         self.gravity = 0.8
 
-    def update(self, userInput):
+    def update(self, userinput):
         if self.ninja_duck:
             self.duck()
         if self.ninja_run:
@@ -95,9 +93,9 @@ class Ninja:
         if self.ninja_jump:
             self.jump()
 
-        if userInput[pygame.K_LEFT]:
+        if userinput[pygame.K_LEFT]:
             self.ninja_rect.x -= 10
-        if userInput[pygame.K_RIGHT]:
+        if userinput[pygame.K_RIGHT]:
             self.ninja_rect.x += 10
 
         if self.ninja_rect.x < 0:
@@ -105,25 +103,25 @@ class Ninja:
         if self.ninja_rect.x > SCREENWIDTH - self.ninja_rect.width:
             self.ninja_rect.x = SCREENWIDTH - self.ninja_rect.width
 
-        if userInput[pygame.K_UP] and self.ninja_rect.y == self.y_pos:
+        if userinput[pygame.K_UP] and self.ninja_rect.y == self.y_pos:
             self.ninja_duck = False
             self.ninja_run = False
             self.ninja_jump = True
             self.jump_vel = JUMP_HIGH
             JUMP_SOUND.play()  # Play jump sound
-        elif userInput[pygame.K_SPACE] and self.ninja_rect.y == self.y_pos:
+        elif userinput[pygame.K_SPACE] and self.ninja_rect.y == self.y_pos:
             self.ninja_duck = False
             self.ninja_run = False
             self.ninja_jump = True
             self.jump_vel = JUMP_HIGH
             JUMP_SOUND.play()  # Play jump sound
 
-        elif userInput[pygame.K_DOWN] and not self.ninja_jump:
+        elif userinput[pygame.K_DOWN] and not self.ninja_jump:
             self.ninja_duck = True
             self.ninja_run = False
             self.ninja_jump = False
             DUCK_SOUND.play()  # Play duck sound
-        elif not (self.ninja_jump or userInput[pygame.K_DOWN]):
+        elif not (self.ninja_jump or userinput[pygame.K_DOWN]):
             self.ninja_duck = False
             self.ninja_run = True
             self.ninja_jump = False
@@ -167,8 +165,8 @@ class Ninja:
         else:
             self.run()
 
-    def draw(self, SCREEN):
-        SCREEN.blit(self.image, (self.ninja_rect.x, self.ninja_rect.y))
+    def draw(self, screen):
+        screen.blit(self.image, (self.ninja_rect.x, self.ninja_rect.y))
 
 class Shuriken:
     def __init__(self):
@@ -184,8 +182,8 @@ class Shuriken:
             self.rect.x = SCREENWIDTH
             self.rect.y = random.randint(100, SCREENHEIGHT - 150)
 
-    def draw(self, SCREEN):
-        SCREEN.blit(self.image, (self.rect.x, self.rect.y))
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
 
 class Kunai:
     def __init__(self):
@@ -201,34 +199,35 @@ class Kunai:
             self.rect.x = SCREENWIDTH
             self.rect.y = random.randint(100, SCREENHEIGHT - 150)
 
-    def draw(self, SCREEN):
-        SCREEN.blit(self.image, (self.rect.x, self.rect.y))
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
 
-def draw_background(SCREEN, bg_x):
-    SCREEN.blit(BACKGROUND, (bg_x, 0))
-    SCREEN.blit(BACKGROUND, (bg_x + BACKGROUND.get_width(), 0))
+def draw_background(screen, bg_x):
+    screen.blit(BACKGROUND, (bg_x, 0))
+    screen.blit(BACKGROUND, (bg_x + BACKGROUND.get_width(), 0))
 
-def draw_ground(SCREEN, ground_x, ground_y):
-    SCREEN.blit(GROUND, (ground_x, ground_y))
-    SCREEN.blit(GROUND, (ground_x + GROUND.get_width(), ground_y))
+def draw_ground(screen, ground_x, ground_y):
+    screen.blit(GROUND, (ground_x, ground_y))
+    screen.blit(GROUND, (ground_x + GROUND.get_width(), ground_y))
 
-def draw_clouds(SCREEN, cloud_x, cloud_y):
-    SCREEN.blit(CLOUDS, (cloud_x, cloud_y))
-    SCREEN.blit(CLOUDS, (cloud_x + CLOUDS.get_width(), cloud_y))
+def draw_clouds(screen, cloud_x, cloud_y):
+    screen.blit(CLOUDS, (cloud_x, cloud_y))
+    screen.blit(CLOUDS, (cloud_x + CLOUDS.get_width(), cloud_y))
 
-def draw_sun(SCREEN):
-    SCREEN.blit(SUN_IMG, (SCREENWIDTH // 2 - 200, SCREENHEIGHT // 2 - 125))
+def draw_sun(screen):
+    screen.blit(SUN_IMG, (SCREENWIDTH // 2 - 200, SCREENHEIGHT // 2 - 125))
 
-def draw_game_over(SCREEN):
+def draw_game_over(screen):
     game_over_text = FONT.render('                ', True, (255, 0, 0))
     restart_button_text = FONT.render('Press Enter', True, (0, 0, 0))
     restart_button = pygame.Rect(SCREENWIDTH // 2 - 125, SCREENHEIGHT // 2 + 100, 250, 50)
-    pygame.draw.rect(SCREEN, (250, 250, 250), restart_button)
-    SCREEN.blit(game_over_text, (SCREENWIDTH // 2 - game_over_text.get_width() // 2, (SCREENHEIGHT // 2) + 75 - game_over_text.get_height() // 2))
-    SCREEN.blit(restart_button_text, (SCREENWIDTH // 2 - restart_button_text.get_width() // 2, (SCREENHEIGHT // 2) + 120 - restart_button_text.get_height() // 2))
+    pygame.draw.rect(screen, (250, 250, 250), restart_button)
+    screen.blit(game_over_text, (SCREENWIDTH // 2 - game_over_text.get_width() // 2, (SCREENHEIGHT // 2) + 75 - game_over_text.get_height() // 2))
+    screen.blit(restart_button_text, (SCREENWIDTH // 2 - restart_button_text.get_width() // 2, (SCREENHEIGHT // 2) + 120 - restart_button_text.get_height() // 2))
     return restart_button
 
 def main():
+    global restart_button, elapsed_time_ms
     run = True
     game_over = False
     clock = pygame.time.Clock()
